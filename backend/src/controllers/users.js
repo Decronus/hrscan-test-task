66137adde2394ee4fc629772b08a42ffdcb40182
Decronus <zick3333@mail.ru>
2023-05-22 12:59:33 +0200

    controllers ref

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,23 +1,23 @@
 const User = require("../models/user");
 
-const getAllUsers = (req, res) => {
-    return User.find()
-        .then((users) => {
-            res.status(200).send(users);
-        })
-        .catch(() => res.status(500).send("Internal server error"));
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).send(users);
+    } catch {
+        res.status(500).send("Internal server error");
+    }
 };
 
 const loginUser = async (req, res) => {
-    const { email } = req.body;
-    const { password } = req.body;
+    const { email, password } = req.body;
     const user = await User.findOne({ email: email });
     if (!user) {
-        res.status(500).send("User with this email does not exists");
+        res.status(400).send("User with this email does not exists");
         return;
     }
     if (user.password !== password) {
-        res.status(500).send("Incorrect password");
+        res.status(400).send("Incorrect password");
         return;
     }
     user.password = undefined;
@@ -28,15 +28,17 @@ const createUser = async (req, res) => {
     const { email } = req.body;
     const user = await User.findOne({ email: email });
     if (user) {
-        res.status(409).send("User with this email already exists");
+        res.status(400).send("User with this email already exists");
         return;
     }
-    return User.create({ ...req.body })
-        .then((user) => {
-            user.password = undefined;
-            res.status(201).send(user);
-        })
-        .catch(() => res.status(500).send("Internal server error"));
+
+    try {
+        const user = await User.create({ ...req.body });
+        user.password = undefined;
+        res.status(201).send(user);
+    } catch {
+        res.status(500).send("Internal server error");
+    }
 };
 
 const updateUser = async (req, res) => {
@@ -44,10 +46,11 @@ const updateUser = async (req, res) => {
     const { id } = req.params;
     const user = await User.findByIdAndUpdate(id, req.body, { new: true });
     if (!user) {
-        res.status(404).send("User with this id does not exists");
+        res.status(400).send("User with this id does not exists");
+    } else {
+        user.password = undefined;
+        res.status(200).send(user);
     }
-    user.password = undefined;
-    res.status(200).send(user);
 };
 
 const uploadPhoto = async (req, res) => {
@@ -55,10 +58,11 @@ const uploadPhoto = async (req, res) => {
     const update = { photoLink: req.file.path };
     const user = await User.findByIdAndUpdate(id, update, { new: true });
     if (!user) {
-        res.status(404).send("User with this id does not exists");
+        res.status(400).send("User with this id does not exists");
+    } else {
+        user.password = undefined;
+        res.status(200).send(user);
     }
-    user.password = undefined;
-    res.status(200).send(user);
 };
 
 module.exports = {
